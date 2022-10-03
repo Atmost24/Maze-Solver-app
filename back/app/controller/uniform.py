1d1f3870dca224ae5e0baeeed72c15e38b8e7487
Juan Sanchez <jusanchez@unal.edu.co>
2022-10-03 12:29:29 -0500

    trees

--- back/app/controller/uniform.py
+++ back/app/controller/uniform.py
@@ -3,7 +3,7 @@ import random
 import sys
 from app.controller.specialTuple import Tup
 from app.controller.maze_solver import convertMaze
-
+import graphviz
 class Uniform:
     def __init__(self, origin, goal, maze):
         self.allPaths = []
@@ -17,7 +17,15 @@ class Uniform:
         if self.isInRange(origin):
             self.origin = origin
         else:
-            self.origin = (0, 0)    
+            self.origin = (0, 0)
+        self.graph=True if self.height<=10 and self.width<=10 else False
+        if self.graph:
+            self.f = graphviz.Digraph('a', format='png')
+            self.f.attr(bgcolor='#282C34')
+            self.f.attr('node',fontcolor='#F9F9F9')
+            self.f.attr('node',color='#F9F9F9')
+            self.f.attr('edge',color='#F9F9F9')
+            self.f.attr('edge',fontcolor='#F9F9F9') 
     def isInRange(self,cell):
         return cell[0] <= self.height and cell[0] >= 0 and cell[1] <= self.width and cell[1] >= 0
     def d(self, start, final):
@@ -45,6 +53,15 @@ class Uniform:
             current = cameFrom[current[0]][current[1]]
             path.append({"x":current[1],"y": current[0]})
         return path[::-1]
+    def calculeDirection(self, curr, prev):
+        if curr[0] - prev[0] > 0:
+            return "u"
+        if curr[0] - prev[0] < 0:
+            return "d"
+        if curr[1] - prev[1] > 0:
+            return "r"
+        if curr[1] - prev[1] < 0:
+            return "l" 
     def findPath(self):
         cameFrom = [[ None for col in range(self.width)] for row in range(self.height)]
         g = [[ sys.maxsize for col in range(self.width)] for row in range(self.height)]
@@ -53,6 +70,7 @@ class Uniform:
         #(f,x,y)
         listAllPaths=[]
         heappush(openSet, Tup(g[self.origin[0]][self.origin[1]], self.origin))
+        self.f.node('00', 'Origin')
         while len(openSet) > 0:
             currCell = heappop(openSet).getPair()
             #self.allPaths.append(self.getPath(cameFrom, currCell))
@@ -68,5 +86,13 @@ class Uniform:
                     cameFrom[neighbour[0]][neighbour[1]] = currCell
                     g[neighbour[0]][neighbour[1]] = preG
                     if preNeighbour not in openSet:
+                        if self.graph:
+                            move = self.calculeDirection(currCell, neighbour)
+                            if neighbour == self.goal:
+                                self.f.attr('node', shape='doublecircle')
+                            self.f.node(f'{neighbour[0]}{neighbour[1]}', f'x:{neighbour[0]}y:{neighbour[1]}' + "\nPeso: \n" + str(g[currCell[0]][currCell[1]]))
+                            self.f.edge(f'{currCell[0]}{currCell[1]}', f'{neighbour[0]}{neighbour[1]}', label=move)
                         heappush(openSet, Tup(g[neighbour[0]][neighbour[1]], neighbour))
+        if self.graph:
+            self.f.render().replace('\\', '/')
         return False  
