f881a8d50a7c8952b96948aefa85e89a53010ff1
Juan Sanchez <jusanchez@unal.edu.co>
2022-10-03 12:45:51 -0500

    trees

--- back/app/controller/astar.py
+++ back/app/controller/astar.py
@@ -94,7 +94,7 @@ class AStar:
                             if neighbour == self.goal:
                                 self.f.attr('node', shape='doublecircle')
                             self.f.node(f'{neighbour[0]}{neighbour[1]}', f'x:{neighbour[0]}y:{neighbour[1]}' + "\nPeso: \n" + str(f[currCell[0]][currCell[1]]))
-                            self.f.edge(f'{currCell[0]}{currCell[1]}', f'{neighbour[0]}{neighbour[1]}', label=move)
+                            self.ff.edge(f'{currCell[0]}{currCell[1]}', f'{neighbour[0]}{neighbour[1]}', label=move)
                         heappush(openSet, Tup(f[neighbour[0]][neighbour[1]], neighbour))
         if self.graph:
             self.f.render().replace('\\', '/')
